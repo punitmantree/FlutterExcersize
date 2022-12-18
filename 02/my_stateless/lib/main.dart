148a7e15ecd6fc26a75b8d632c9cbcd3480697b4
Punit Maheshwari <punitmaheshwari143@gmail.com>
2022-12-18 18:32:21 +0530

    Added 2 exercize files(01 AND 02)

---
+++ 02/my_stateless/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var app = MaterialApp(
+    title: 'Flutter App',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.amber),
+    ),
+    home: Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text('Flutter'),
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("I am at line one"),
+          const Text("I am at line number 2"),
+          ElevatedButton(
+            onPressed: onPressed,
+            style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.blue,
+                shadowColor: Colors.orangeAccent),
+            child: const Text("Login"),
+          )
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(
+          Icons.add_a_photo,
+          color: Colors.black,
+        ),
+      ),
+    ),
+  );
+  runApp(app);
+}
+
+void onPressed() {}
